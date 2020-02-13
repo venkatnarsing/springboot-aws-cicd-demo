f5250143db9654b21b8ec3dff86d9c7ada579954
Venkateshwarlu Narsing <venarsin@VENARSIN-45R1W.partnet.cisco.com>
2020-02-13 17:25:29 +0530

    modified

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "installing dependencies"
-#apt-get install openjdk-8-jdk
-#yum install java-1.8.0-openjdk-devel
+sudo yum install -y java-1.8.0-openjdk-devel
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "from start server"
-java -jar demo.jar > /dev/null 2> /dev/null < /dev/null &
+java -jar demo.jar > /dev/null 2>&1
