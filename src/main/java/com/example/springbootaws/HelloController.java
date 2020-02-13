e3b17cbfdee702ab67d517b6388d7b62e386dad1
Venkateshwarlu Narsing <venarsin@VENARSIN-45R1W.partnet.cisco.com>
2020-02-13 14:13:32 +0530

    modified

--- src/main/java/com/example/springbootaws/HelloController.java
+++ src/main/java/com/example/springbootaws/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 	@RequestMapping("/hello/{name}")
     public String helloDocker(@PathVariable(value = "name") String name) {
-        String response = "Welcome::: " + name + " Response received on : " + new Date();
+        String response = "Welcome::: " + name + "\nResponse received on : " + new Date();
         System.out.println(response);
         return response;
  
